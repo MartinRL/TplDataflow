5871dc8e057c42c874f0b05e7fbff777eb10cb4e
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-09-04 14:29:27 +0200

    add logic

--- TheIliadOfHomerPipeline/Program.cs
+++ TheIliadOfHomerPipeline/Program.cs
@@ -11,5 +11,62 @@ static class Program
 {
     static void Main()
     {
+        //
+        // Create the members of the pipeline.
+        //
+
+        // Downloads the requested resource as a string.
+        var downloadString = new TransformBlock<string, string>(async uri =>
+        {
+            Console.WriteLine("Downloading '{0}'...", uri);
+
+            return await new HttpClient(new HttpClientHandler { AutomaticDecompression = System.Net.DecompressionMethods.GZip }).GetStringAsync(uri);
+        });
+
+        // Separates the specified text into an array of words.
+        var createWordList = new TransformBlock<string, string[]>(text =>
+        {
+            Console.WriteLine("Creating word list...");
+
+            // Remove common punctuation by replacing all non-letter characters
+            // with a space character.
+            char[] tokens = text.Select(c => char.IsLetter(c) ? c : ' ').ToArray();
+            text = new string(tokens);
+
+            // Separate the text into an array of words.
+            return text.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        });
+
+        // Removes short words and duplicates.
+        var filterWordList = new TransformBlock<string[], string[]>(words =>
+        {
+            Console.WriteLine("Filtering word list...");
+
+            return words
+               .Where(word => word.Length > 3)
+               .Distinct()
+               .ToArray();
+        });
+
+        // Finds all words in the specified collection whose reverse also
+        // exists in the collection.
+        var findReversedWords = new TransformManyBlock<string[], string>(words =>
+        {
+            Console.WriteLine("Finding reversed words...");
+
+            var wordsSet = new HashSet<string>(words);
+
+            return from word in words.AsParallel()
+                   let reverse = new string(word.Reverse().ToArray())
+                   where word != reverse && wordsSet.Contains(reverse)
+                   select word;
+        });
+
+        // Prints the provided reversed words to the console.
+        var printReversedWords = new ActionBlock<string>(reversedWord =>
+        {
+            Console.WriteLine("Found reversed words {0}/{1}",
+               reversedWord, new string(reversedWord.Reverse().ToArray()));
+        });
     }
 }
