9d28bd30d1f050a9b9a50ee7c76b8473074cf43f
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-09-04 14:08:11 +0200

    create the basic application

--- TheIliadOfHomerPipeline/Program.cs
+++ TheIliadOfHomerPipeline/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks.Dataflow;
+
+// Demonstrates how to create a basic dataflow pipeline.
+// This program downloads the book "The Iliad of Homer" by Homer from the Web
+// and finds all reversed words that appear in that book.
+static class Program
+{
+    static void Main()
+    {
+    }
+}
