9ff374085613b7ee0277b97d283d1770d9b59f61
Martin Rosén-Lidholm <martin.rosenlidholm@gmail.com>
2024-09-08 14:14:08 +0200

    :lipstick: remove unused usings

--- CompositeImages/Form1.cs
+++ CompositeImages/Form1.cs
@@ -1,13 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
-using System.Windows.Forms;
 
 namespace CompositeImages;
 
